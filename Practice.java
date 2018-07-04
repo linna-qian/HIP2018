1acc47e7ffe46a7e1a80ef2707e3014e12f5d7f9
linna-qian <linnaqian776@gmail.com>
2018-07-04 08:54:41 -0600

    Add Test 2

--- Practice.java
+++ Practice.java
@@ -3,4 +3,5 @@ public static void main(String[] args){
     System.out.println("Puppies are cute!");
     System.out.println("Cats are cute!");
     System.out.println("Dogs are cute!");
+    System.out.println("Test 2");
 }
